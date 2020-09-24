72e9531b952a6ba432f5774639b9ec99d2e5b1fb
sadSack <prbillingsley89@gmail.com>
2020-09-24 14:54:52 -0500

    Add Display Functionality
    *Add Display object in Javascript
    *Add onClearButton event listener in Javascript
    *Add black class in CSS for numberBlocks

--- app.js
+++ app.js
@@ -17,13 +17,167 @@ const memory = {
         console.log(this.previousNumber);
     },
     parseCurrentNumber() {
-        this.currentNumber = parseFloat(this.currentNumber);
+        if(this.currentNumber) {
+            this.currentNumber = parseFloat(this.currentNumber);
+        }
     },
     updateOperation(symbol) {
         this.operation = symbol;
         console.log(symbol);
+    },
+    // clearCurrentNumber() {
+    //     this.currentNumber = '';
+    // },
+    // clearPreviousNumber() {
+    //     this.previousNumber = '';
+    // },
+    // clearOperation() {
+    //     this.operation = undefined;
+    // },
+    clear() {
+        if(this.currentNumber) {
+            this.currentNumber = '';
+            console.log('currentNumber is ' + this.currentNumber + ' previousNumber is ' + this.previousNumber + ' operation is ' + this.operation);
+            return;
+        } else if(this.previousNumber) {
+            this.previousNumber = '';
+            this.operation = undefined;
+            console.log('currentNumber is ' + this.currentNumber + ' previousNumber is ' + this.previousNumber + ' operation is ' + this.operation);
+            return;
+        } 
     }
 };
+const display = {
+    numberArray: ['','','','','','','',''],
+    resetNumberArray() {
+        this.numberArray = ['','','','','','','',''];
+    },
+    updateNumberArray() {
+        const displayNumber = memory.currentNumber.split('');
+        this.numberArray.shift();
+        this.numberArray.push(displayNumber[displayNumber.length - 1]);
+    },
+    updateDisplay() {
+        this.clearAll();
+        this.updateNumberArray();
+        console.log(this.numberArray);
+        for(let i = this.numberArray.length - 1; i >= 0; i--) {
+            if(this.numberArray[i] === '1') {
+                this.generateOne(i);
+            }
+            if(this.numberArray[i] === '2') {
+                this.generateTwo(i);
+            }
+            if(this.numberArray[i] === '3') {
+                this.generateThree(i);
+            }
+            if(this.numberArray[i] === '4') {
+                this.generateFour(i);
+            }
+            if(this.numberArray[i] === '5') {
+                this.generateFive(i);
+            }
+            if(this.numberArray[i] === '6') {
+                this.generateSix(i);
+            }
+            if(this.numberArray[i] === '7') {
+                this.generateSeven(i);
+            }
+            if(this.numberArray[i] === '8') {
+                this.generateEight(i);
+            }
+            if(this.numberArray[i] === '9') {
+                this.generateNine(i);
+            }
+            if(this.numberArray[i] === '0') {
+                this.generateZero(i);
+            }
+        }
+    },
+    clearBlock(digitPlace) {
+        numberBlocks[digitPlace].children[0].classList.remove('black');
+        numberBlocks[digitPlace].children[1].classList.remove('black');
+        numberBlocks[digitPlace].children[2].classList.remove('black');
+        numberBlocks[digitPlace].children[3].classList.remove('black');
+        numberBlocks[digitPlace].children[4].classList.remove('black');
+        numberBlocks[digitPlace].children[5].classList.remove('black');
+        numberBlocks[digitPlace].children[6].classList.remove('black');
+    },
+    clearAll() {
+        for(let i = 0; i < 8; i ++) {
+            this.clearBlock(i);
+        }
+    },
+    generateOne(digitPlace) {
+        numberBlocks[digitPlace].children[2].className += ' black';
+        numberBlocks[digitPlace].children[5].className += ' black';
+    },
+    generateTwo(digitPlace) {
+        numberBlocks[digitPlace].children[0].className += ' black';
+        numberBlocks[digitPlace].children[2].className += ' black';
+        numberBlocks[digitPlace].children[3].className += ' black';
+        numberBlocks[digitPlace].children[4].className += ' black';
+        numberBlocks[digitPlace].children[6].className += ' black';
+    },
+    generateThree(digitPlace) {
+        numberBlocks[digitPlace].children[0].className += ' black';
+        numberBlocks[digitPlace].children[2].className += ' black';
+        numberBlocks[digitPlace].children[3].className += ' black';
+        numberBlocks[digitPlace].children[5].className += ' black';
+        numberBlocks[digitPlace].children[6].className += ' black';
+    },
+    generateFour(digitPlace) {
+        numberBlocks[digitPlace].children[1].className += ' black';
+        numberBlocks[digitPlace].children[2].className += ' black';
+        numberBlocks[digitPlace].children[3].className += ' black';
+        numberBlocks[digitPlace].children[5].className += ' black';
+    },
+    generateFive(digitPlace) {
+        numberBlocks[digitPlace].children[0].className += ' black';
+        numberBlocks[digitPlace].children[1].className += ' black';
+        numberBlocks[digitPlace].children[3].className += ' black';
+        numberBlocks[digitPlace].children[5].className += ' black';
+        numberBlocks[digitPlace].children[6].className += ' black';
+    },
+    generateSix(digitPlace) {
+        numberBlocks[digitPlace].children[0].className += ' black';
+        numberBlocks[digitPlace].children[1].className += ' black';
+        numberBlocks[digitPlace].children[3].className += ' black';
+        numberBlocks[digitPlace].children[4].className += ' black';
+        numberBlocks[digitPlace].children[5].className += ' black';
+        numberBlocks[digitPlace].children[6].className += ' black';
+    },
+    generateSeven(digitPlace) {
+        numberBlocks[digitPlace].children[0].className += ' black';
+        numberBlocks[digitPlace].children[2].className += ' black';
+        numberBlocks[digitPlace].children[5].className += ' black';
+    },
+    generateEight(digitPlace) {
+        numberBlocks[digitPlace].children[0].className += ' black';
+        numberBlocks[digitPlace].children[1].className += ' black';
+        numberBlocks[digitPlace].children[2].className += ' black';
+        numberBlocks[digitPlace].children[3].className += ' black';
+        numberBlocks[digitPlace].children[4].className += ' black';
+        numberBlocks[digitPlace].children[5].className += ' black';
+        numberBlocks[digitPlace].children[6].className += ' black';
+    },
+    generateNine(digitPlace) {
+        numberBlocks[digitPlace].children[0].className += ' black';
+        numberBlocks[digitPlace].children[1].className += ' black';
+        numberBlocks[digitPlace].children[2].className += ' black';
+        numberBlocks[digitPlace].children[3].className += ' black';
+        numberBlocks[digitPlace].children[5].className += ' black';
+    },
+    generateZero(digitPlace) {
+        numberBlocks[digitPlace].children[0].className += ' black';
+        numberBlocks[digitPlace].children[1].className += ' black';
+        numberBlocks[digitPlace].children[2].className += ' black';
+        numberBlocks[digitPlace].children[4].className += ' black';
+        numberBlocks[digitPlace].children[5].className += ' black';
+        numberBlocks[digitPlace].children[6].className += ' black';
+    }
+};
+
 const compute = function(currentNumber, previousNumber, operation) {
     if(operation === '+') {
         logic.add(previousNumber, currentNumber);
@@ -34,7 +188,7 @@ const compute = function(currentNumber, previousNumber, operation) {
     } else if (operation === '÷') {
         logic.divide(previousNumber, currentNumber);
     }
-}
+};
 
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
@@ -46,10 +200,12 @@ const percentButton = document.querySelector('[data-percent]');
 const memoryRecallButton = document.querySelector('[data-memory-recall]');
 const memoryMinusButton = document.querySelector('[data-memory-minus]');
 const memoryPlusButton = document.querySelector('[data-memory-plus]');
+const numberBlocks = document.querySelectorAll('.screen__number');
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
         memory.updateCurrentNumber(button.innerText);
+        display.updateDisplay();
     })
 })
 operationButtons.forEach(button => {
@@ -61,4 +217,10 @@ operationButtons.forEach(button => {
 equalsButton.addEventListener('click', () => {
     memory.parseCurrentNumber();
     compute(memory.currentNumber, memory.previousNumber, memory.operation);
+})
+onClearButton.addEventListener('click', () => {
+    memory.clear();
+    display.clearAll();
+    display.resetNumberArray();
+    console.log(display.numberArray);
 })
